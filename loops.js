bafb8a66d35ceb3b86e9d984b1f5c2f0dfca944e
Juan Ortega <juanakin0108@gmail.com>
2018-11-01 23:55:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,30 @@
+
+
 function forLoop(array) {
   
+   for ( let i = 0; i < 25; i++) {
+     
+     if (i === 1) {
+       
+       array.push(`I am 1 strange loop.`);
+       
+     } else {
+       
+       array.push(`I am ${i} strange loops.`);
+     }
+  }
   
-  for (let i = 0; i < 25; i++) {
+  return array;
+}
+
+function whileLoop(i) {
+  
+  let countDown = 10;
+  
+  
+  while ( countDown  0) {
     
-    return array.push()
-   
+     console.log('done');
+  }
+  
+}
